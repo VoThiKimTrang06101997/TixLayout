e6eeb72315434f38d4b9882172de517c46628ce5
VoThiKimTrang06101997 <vothikimtrang0610@gmail.com>
2022-06-07 23:42:20 +0700

    TixLayout

---
+++ js/main.js
@@ -0,0 +1,150 @@
+/**
+ * Người tạo: Nguyễn Thành Tuấn
+ * Ngày: 19/2/2021
+ * Version: 1.0
+ */
+
+/**
+ * Magnific-Popup-master
+ */
+
+/**
+ * Carousel: Magnific-Popup-master
+ */
+
+$("#popup-em-la-cua-em").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=-ZBnKx87yyk'
+    },
+    type: 'iframe'
+});
+
+$("#popup-lua-deu-gap-lua-dao").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=T36HGZagV5w'
+    },
+    type: 'iframe'
+});
+
+$("#popup-can-phong-ma-am").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=kEgUgrh2rdA'
+    },
+    type: 'iframe'
+});
+
+/**
+ * Film: Magnific-Popup-master
+ */
+
+$("#tom-jerry").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=fgqEyC19538'
+    },
+    type: 'iframe'
+});
+
+$("#lua-deu-gap-lua-dao-the-con-heartist").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=T36HGZagV5w'
+    },
+    type: 'iframe'
+});
+
+$("#tazza-than-bip-jack-chot").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=gqcpChNYH10'
+    },
+    type: 'iframe'
+});
+
+$("#tay-xa-thu-the-marksman").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=g7LeR7chXVM'
+    },
+    type: 'iframe'
+});
+
+$("#ainbo-nu-chien-binh-amazon").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=Nm4xsyTEVNU'
+    },
+    type: 'iframe'
+});
+
+$("#sieu-trom-duong-pho").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=3kjiWrFeYhg'
+    },
+    type: 'iframe'
+});
+
+$("#nam-sinh-so-11").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=Lz1CN4OdO_s'
+    },
+    type: 'iframe'
+});
+
+$("#gai-gia-lam-chieu-v-nhung-cuoc-doi-vuong-gia").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=8b4jXgpO7vY'
+    },
+    type: 'iframe'
+});
+
+$("#tho-san-quai-vat-monster-hunter").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=iCw-LfLw_Zk'
+    },
+    type: 'iframe'
+});
+
+$("#cua-lai-vo-bau").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=7V7SBjaQQ4g'
+    },
+    type: 'iframe'
+});
+
+$("#thuyen-truong-rang-kiem-va-vien-kim-cuong-ma-thuat").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=935dFLgEwfU'
+    },
+    type: 'iframe'
+});
+
+$("#can-phong-ma-am-stigmatized-properties").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=kEgUgrh2rdA'
+    },
+    type: 'iframe'
+});
+
+$("#di-nguyen-bi-an").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=4zYGlqyCVk4'
+    },
+    type: 'iframe'
+});
+
+$("#vung-troi-tu-than-horizon-line").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=Lc8Y3pxNuqc'
+    },
+    type: 'iframe'
+});
+
+$("#ban-quy-come-play").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=LQwiqhdMQ7g'
+    },
+    type: 'iframe'
+});
+
+$("#cuoc-giai-cuu-sinh-tu").magnificPopup({
+    items: {
+        src: 'https://www.youtube.com/watch?v=qZBOe2is_M4'
+    },
+    type: 'iframe'
+});
